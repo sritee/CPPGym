a7d0fe02d374343a0e5baf4fdbbe96d54cd3dcfa
sritee <thiagarajan14111@mech.ssn.edu.in>
2019-09-29 01:49:31 -0700

    invalid action exception

---
+++ gym/action.h
@@ -0,0 +1,5 @@
+#ifndef ACTION
+#define ACTION
+
+#endif // ACTION
+
--- gym/exceptions.h
+++ gym/exceptions.h
@@ -9,5 +9,12 @@ namespace gym_exception
     public:
         NotImplementedError() : std::logic_error("Function is not implemented") { }
     };
+
+    class InvalidAction : public std::logic_error
+    {
+        public:
+
+            InvalidAction() : std::logic_error("The Action is out of bounds for this environment") { }
+    };
 }
 
